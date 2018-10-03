6c24d8e5642214da85f4db39cd451a0181d80071
Namsons Davis <davis.namsons@tieto.com>
2018-10-03 13:52:31 +0300

    yes

--- test/filters_test.rb
+++ test/filters_test.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'test_helper'
 
 class FiltersTest < Haml::TestCase
@@ -184,6 +185,12 @@ class JavascriptFilterTest < Haml::TestCase
     refute_match('//<![CDATA[', out)
     refute_match('//]]>', out)
   end
+
+  test "should emit tag on empty block" do
+    html = "<script>\n  \n</script>\n"
+    haml = ":javascript"
+    assert_equal(html, render(haml))
+  end
 end
 
 class CSSFilterTest < Haml::TestCase
@@ -222,6 +229,12 @@ class CSSFilterTest < Haml::TestCase
     out = render(haml, :format => :html5)
     refute_match('<![CDATA[', out)
     refute_match(']]>', out)
+  end
+
+  test "should emit tag on empty block" do
+    html = "<style>\n  \n</style>\n"
+    haml = ":css"
+    assert_equal(html, render(haml))
   end
 end
 
